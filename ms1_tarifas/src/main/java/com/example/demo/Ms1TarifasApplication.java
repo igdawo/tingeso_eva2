eae416bc5913ff7d792bd1b652fed79cfc3d6a56
Ignacio D'Agostino <ignacio.dagostino@usach.cl>
2025-06-06 16:48:04 -0400

    el ocomiit mas epico de la istoria

---
+++ ms1_tarifas/src/main/java/com/example/demo/Ms1TarifasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms1TarifasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms1TarifasApplication.class, args);
+	}
+
+}
